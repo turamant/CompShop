58756ac7080d579ff55edd3a6f91b1749060abed
askvart <tur1amant@gmail.com>
2021-10-28 18:13:32 +0300

    vendor| models.py| class Vendor, User

--- vendor/models.py
+++ vendor/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Vendor(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.OneToOneField('User', related_name='vendor', on_delete=models.CASCADE)
+    created_by = models.OneToOneField(User, related_name='vendor', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['name']
