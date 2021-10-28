3de67b4e02ce08904803a640492612cea91a1f6e
askvart <tur1amant@gmail.com>
2021-10-28 18:33:28 +0300

    compshop|urls.py| path(vendors/)

--- compshop/urls.py
+++ compshop/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vendors/', include('vendors.url')),
     path('', include('core.urls')),
+
 ]
